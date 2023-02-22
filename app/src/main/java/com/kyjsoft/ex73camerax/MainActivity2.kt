410ea241f18af363831a307a1cc216a03db107eb
keemyujeong <thinkrightkim@gmail.com>
2023-02-22 22:11:23 +0900

    Initial commit

---
+++ app/src/main/java/com/kyjsoft/ex73camerax/MainActivity2.kt
@@ -0,0 +1,168 @@
+package com.kyjsoft.ex73camerax
+
+import android.Manifest
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.view.PreviewView
+import android.widget.ImageView
+import android.os.Bundle
+import com.kyjsoft.ex73camerax.R
+import android.view.WindowManager
+import android.view.View
+import android.os.Build
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.camera.lifecycle.ProcessCameraProvider
+import java.lang.Runnable
+import androidx.camera.core.Preview
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import java.util.concurrent.ExecutionException
+import java.lang.InterruptedException
+import androidx.core.content.ContextCompat
+import android.content.ContentValues
+import android.provider.MediaStore
+import android.net.Uri
+import com.bumptech.glide.Glide
+import androidx.camera.core.ImageCaptureException
+import java.text.SimpleDateFormat
+
+class MainActivity2 : AppCompatActivity() {
+    var previewView: PreviewView? = null
+    var iv: ImageView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 액티비티의 영역을 외부 상태표시줄이랑 물리버튼 영역까지 확대하는 플래그(설정) 값이 있음
+        window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        previewView = findViewById(R.id.preview_view)
+        iv = findViewById(R.id.iv)
+        findViewById<View>(R.id.fab_image_capture).setOnClickListener { view: View? -> clickCapture() } // 카메라가 액티비티 생명주기랑 연결된 후 버튼 누르면 캡쳐본 뜨게
+
+        // 동적 퍼미션들
+        var permissions: Array<String?>? =
+            null //28 버전이상인거랑 낮은 거랑 구분 할라고 미리 안주는 거임.-> 28 이상은 write 안줘도 됨.
+        permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+        ) else arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        if (checkSelfPermission(permissions[0]!!) == PackageManager.PERMISSION_DENIED) { // 원래는 3개 다 해야함 -> 구글에 다 물어보는 메소드 있음.
+            requestPermissions(permissions, 10)
+        } else {
+            // 프리뷰를 시작한다.
+            startPreview()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 10) {
+            for (grantresult in grantResults) {
+                if (grantresult == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(this, "카메라 사용 기능 불가", Toast.LENGTH_SHORT).show()
+                    finish() // 앱이 꺼지고
+                    return  // 끝남
+                }
+            }
+            startPreview() // 여기까지 오면 퍼미션이 다 허용되었다는 것이므로
+        }
+    }
+
+    fun startPreview() {
+
+        // 1. 카메라의 셔터 조리개가 open/close되는 순간이 액티비티의 라이프사이클과 같도록 연결하는 작업 -> 앱이 꺼지고 카메라도 꺼지게
+        val listenableFuture = ProcessCameraProvider.getInstance(this) // 연결결과를 들을 수 있는 녀석
+        // 2. preview 준비가 가능함을 알려주는 리스너(별도 스레드로 작업은 하되 -> main이 작업하게 하는 거임.)
+//        listenableFuture.addListener( () -> { // 별도 스레드는 파라미터가 없어서 걍 ()
+
+//        }, ContextCompat.getMainExecutor(this));
+        listenableFuture.addListener({
+            try {
+                // 카메라 기능 객체 가져오기
+                val cameraProvider = listenableFuture.get()
+
+                // 프리뷰 객체 생성
+                val preview = Preview.Builder().build()
+                // 프리뷰 객체가 사용할 고속 버퍼 뷰(SurfaceView, 화면을 빠르게 잡아내는 뷰) 설정
+                preview.setSurfaceProvider(previewView!!.surfaceProvider)
+
+                // 디바이스에 있는 카메라 중 하나를 선택
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+                cameraProvider.unbindAll() // 혹시 기존에 연결되어있는 카메라 기능들을 제거하고 내 앱 생명주기에 맞춰서 카메라 preview를 제어하도록
+
+                // bind 하기 전에 이미지 캡쳐를 하는 객체 생성하기
+                imageCapture = ImageCapture.Builder().build()
+
+                // preview랑 imageCapture 둘다 생명 주기 에 붙여줌
+                cameraProvider.bindToLifecycle(
+                    this@MainActivity2,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            } catch (e: ExecutionException) {
+                e.printStackTrace()
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    // 이미지 캡쳐 객체 참조변수(안드로이드 권장) -> 얘 객체를 preview하는 곳에서 생성함.
+    var imageCapture: ImageCapture? = null
+    fun clickCapture() {
+        if (imageCapture == null) return
+
+        // 캡쳐한 사진을 파일로 저장할 파일명만들기 -> 날짜를 이용한
+        val filename =
+            SimpleDateFormat("yyyyMMddHHmmss").format(System.currentTimeMillis()) // 자바 방법
+        // 사진 정보가 저장된 MediaStore(미디어 저장소)의 DB에 넣을 값들(1개의 record)를 가진 객체
+        val contentValues = ContentValues()
+        contentValues.put(
+            MediaStore.MediaColumns.DISPLAY_NAME,
+            filename
+        ) // db 특정 하나의 칸에 filename이라고 값 넣기
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg") // 확장자 타입 정해주기
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) contentValues.put(
+            MediaStore.MediaColumns.RELATIVE_PATH,
+            "Pictures/CameraX-Image"
+        )
+
+        // 이미지 캡쳐가 찍은 사진을 저장하는 경로를 관리하는 객체 만들기
+        val outputFileOptions = ImageCapture.OutputFileOptions.Builder(
+            contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues
+        ).build()
+        // 다른 앱에서 준거(db)를 쓸 때 getContentResolver() -> 원래 있는 db에서 가져오는 거니까
+
+
+        // 이미지 캡쳐에게 사진을 취득하라고 명령하기
+        imageCapture!!.takePicture(
+            outputFileOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Toast.makeText(this@MainActivity2, "촬영 성공", Toast.LENGTH_SHORT).show()
+
+                    // 저장될 파일의 uri
+                    val uri = outputFileResults.savedUri
+                    Glide.with(this@MainActivity2).load(uri).into(iv!!)
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        this@MainActivity2,
+                        "캡쳐 실패 :" + exception.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+    } // 실디바이스밖에 안됨.
+}
